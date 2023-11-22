29ac7feff35e6ad31b4a71cd2f8c9cfb491f3399
willzer0 <pembunuh4444@gmail.com>
2023-11-22 19:11:35 +0700

    check

--- src/components/VisiMisi/VisiMisi.jsx
+++ src/components/VisiMisi/VisiMisi.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { getDatabase, ref, onValue } from "firebase/database";
 // import axios from "axios";
 
-
 function VisiMisi() {
   const [misi, setMisi] = useState("");
   const [visi, setVisi] = useState("");
@@ -13,34 +12,32 @@ function VisiMisi() {
     onValue(featuresRef, (snapshot) => {
       const data = snapshot.val();
       // if (data) {
-        setMisi(data.misi);
-        setVisi(data.visi);
+      setMisi(data.misi);
+      setVisi(data.visi);
 
-        // console.log(data);
+      // console.log(data);
       // }
     });
     // axios
     //   .get("http://localhost:3000/profile")
     //   .then((res) => setFeatures(res.data));
-  }, []);
+  }, []);
   return (
     <section className="page-section" id="services">
       <div className="container">
-
-            <div className="row text-center ">
-              {" "}
-              <center>
-                <div className="col-md-4">
-                  <h4 className="my-3">Misi</h4>
-                  <p className="text-muted">{misi} </p>
-                </div>
-                <div className="col-md-4">
-                  <h4 className="my-3">Visi</h4>
-                  <p className="text-muted">{visi} </p>
-                </div>
-              </center>
+        <div className="row text-center ">
+          {" "}
+          <center>
+            <div className="col-md-4">
+              <h4 className="my-3">Misi</h4>
+              <p className="text-muted">{misi} </p>
             </div>
-
+            <div className="col-md-4">
+              <h4 className="my-3">Visi</h4>
+              <p className="text-muted">{visi} </p>
+            </div>
+          </center>
+        </div>
       </div>
     </section>
   );
