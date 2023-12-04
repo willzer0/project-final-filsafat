d3e0b91055f70514a992fed0479caac27b2b1b62
unseven13 <j13karongkong@gmail.com>
2023-12-04 08:23:22 +0800

    ganti warna button

--- src/components/VisiMisi/VisiMisi.jsx
+++ src/components/VisiMisi/VisiMisi.jsx
@@ -33,7 +33,7 @@ function VisiMisi() {
               <p className="text-muted">{misi} </p>
             </div>
             <div className="col-md-4">
-              <h4 className="my-3">Vission</h4>
+              <h4 className="my-3">Vision</h4>
               <p className="text-muted">{visi} </p>
             </div>
           </center>
